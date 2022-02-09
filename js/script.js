74022efd83435f0af83ba5c41662abeb7171f8aa
yamarashi <lualgoritmo@outlook.com>
2022-02-09 20:19:12 -0300

    Finalizado Consumação API Pokemon

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const getPokemon = async() =>
+{
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=200`);
+    return response;
+}
+
+const $poker = document.querySelector('.name');
+
+const personPokemon = async() =>
+{
+    const pokemonResult = await  getPokemon();
+    pokemonResult.json()
+    .then(person => {
+        for(let data in person)
+        {
+          person.results.map(response => {
+              let li = document.createElement('li');
+                  li.innerHTML = `${response.name},  ${response.url}`;
+                  $poker.append(li);
+          });
+        }
+    })
+    .catch(error => console.log(error));
+}
+personPokemon();
