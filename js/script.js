36c5207aea94bddb24cd85be998bcd04447df50d
yamarashi <lualgoritmo@outlook.com>
2022-02-11 15:13:56 -0300

    Refatorado

--- js/script.js
+++ js/script.js
@@ -4,16 +4,29 @@ const $poker = document.querySelector('.name');
 const getPokemon = async() =>
 {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=200`);
-    return response;
+    return await response.json();
 }
 
 const personPokemon = async() =>
 {
     const pokemonResult = await  getPokemon();
+    console.log(pokemonResult.results);
+    pokemonResult.results.map(response => 
+    {
+        let li = document.createElement('li');
+            li.innerHTML = `${response.name},  ${response.url}`;
+            $poker.append(li);
+    });
+}
+personPokemon();
+/*const personPokemon = async() =>
+{
+    const pokemonResult = await  getPokemon();
+    console.log(pokemonResult.results);
     pokemonResult.json()
     .then(person => 
     {
-        //console.log('Person',person.results);
+        console.log('Person',person);
           person.results.map(response => 
           {
               let li = document.createElement('li');
@@ -22,5 +35,4 @@ const personPokemon = async() =>
           });
     })
     .catch(error => alert(error));
-}
-personPokemon();
+}*/
