052c4aa97efb8707f89f059bfdee4344e96e7a01
yamarashi <lualgoritmo@outlook.com>
2022-02-19 13:03:52 -0300

    Refatorando com destruct e deixando amostra as outras formas

--- js/script.js
+++ js/script.js
@@ -4,10 +4,22 @@ const $poker = document.querySelector('.name');
 const getPokemon = async() =>
 {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=200`);
+    //console.log('TOTAL',response.json());
     return await response.json();
 }
-
-const personPokemon = async() =>
+const personPokemon = async result=>
+{
+    result = await getPokemon();
+    result.results.map(response =>
+    {
+        const{name, url} = response;
+        let li = document.createElement('li');
+        li.innerHTML = `${name},  ${url}`;
+        $poker.append(li);
+        //console.log(name, url);
+    });
+}
+/*const personPokemon = async() =>
 {
     const pokemonResult = await  getPokemon();
     console.log(pokemonResult.results);
@@ -17,7 +29,7 @@ const personPokemon = async() =>
             li.innerHTML = `${response.name},  ${response.url}`;
             $poker.append(li);
     });
-}
+}*/
 personPokemon();
 /*const personPokemon = async() =>
 {
